8431df5ccde9e045551d0a5b2fb2da6d664b8f6e
UnknownProjects-cloudsystem <hazzanzz@aurevant.ca>
2026-08-08 17:48:47 -0400

    Fix base URL handling in robots function

--- cci-website/src/app/robots.ts
+++ cci-website/src/app/robots.ts
@@ -2,13 +2,17 @@ import type { MetadataRoute } from "next";
 import { site } from "@/lib/site";
 
 export default function robots(): MetadataRoute.Robots {
+  const baseUrl = site.url.replace(/\/$/, "");
+
   return {
     rules: {
       userAgent: "*",
       allow: "/",
       disallow: ["/api/"],
     },
-    sitemap: `${site.url}/sitemap.xml`,
-    host: site.url,
+
+    sitemap: `${baseUrl}/sitemap.xml`,
+
+    host: baseUrl,
   };
 }
